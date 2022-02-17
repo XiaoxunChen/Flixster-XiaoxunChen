6b7cc72711903ae402901350ee70498f9ed82ba3
XiaoxunChen <xiaoxunchen@ucsb.edu>
2022-02-16 20:51:37 -0800

    Assignment2

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by apple on 2022/2/16.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
--- Flixster/MoviesViewController.swift
+++ Flixster/MoviesViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableView.delegate = self
         
         // Do any additional setup after loading the view.
-        print("H")
+       
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -69,6 +69,20 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         cell.posterView.af_setImage(withURL: posterUrl!)
         
         return cell
+    }
+    override func prepare(for segue:
+    UIStoryboardSegue, sender: Any?){
+        print("Loading up the details screen")
+        // Find the selected moive
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        // Pass the selected moive to the details iew controller
+        let detailsViewController = segue.destination as! MoiveDetailsViewController
+        detailsViewController.movie = movie
+    
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
 
